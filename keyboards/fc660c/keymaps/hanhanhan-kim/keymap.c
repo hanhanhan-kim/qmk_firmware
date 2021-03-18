f2ae2ba67e28140fe212cec793922ebf3e432f79
Han Kim <hankim@caltech.edu>
2021-03-18 16:35:18 -0700

    add more macros and add mouse control

--- keyboards/fc660c/keymaps/hanhanhan-kim/keymap.c
+++ keyboards/fc660c/keymaps/hanhanhan-kim/keymap.c
@@ -21,7 +21,8 @@ enum custom_keycodes {
   
     UNTAB_TAB=SAFE_RANGE,
     SFTPSCR_PSCR,
-    HOME_END,
+    END_HOME,
+    MOUSE_R_L,
 
     START_TERMINAL,
 
@@ -156,17 +157,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
       return false; // We handled this keypress
 
-    case HOME_END:
+    case END_HOME:
       // If tapped, will send Home, if held down, will send End
-      if(record->event.pressed) { // When HOME_END is pressed for ANY duration:
+      if(record->event.pressed) { // When END_HOME is pressed for ANY duration:
           my_hash_timer = timer_read();
 
-        } else { // When HOME_END is EVER released
+        } else { // When END_HOME is EVER released
 
-          if (timer_elapsed(my_hash_timer) < TAPPING_TERM) { // If HOME_END was tapped
+          if (timer_elapsed(my_hash_timer) < TAPPING_TERM) { // If END_HOME was tapped
             SEND_STRING(SS_TAP(X_HOME)); 
 
-          } else { // If HOME_END was held down:          
+          } else { // If END_HOME was held down:          
             tap_code(KC_END);
           }
         }
@@ -189,6 +190,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
         }
         break;
+
+    case MOUSE_R_L:
+      // If tapped, will send Home, if held down, will send End
+      if(record->event.pressed) { // When MOUSE_R_L is pressed for ANY duration:
+          my_hash_timer = timer_read();
+
+        } else { // When MOUSE_R_L is EVER released
+
+          if (timer_elapsed(my_hash_timer) < TAPPING_TERM) { // If MOUSE_R_L was tapped
+            tap_code(KC_MS_BTN1); 
+
+          } else { // If MOUSE_R_L was held down:          
+            tap_code(KC_MS_BTN2);
+          }
+        }
+      return false; // We handled this keypress
 
   }
 
@@ -463,17 +480,17 @@ bool led_update_kb(led_t led_state) {
 // KEYMAP-----------------------------------------------------------------------------
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-        TD(ESC_GRAVE_MD_DOCS), KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSPC,     SFTPSCR_PSCR,
-        UNTAB_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,       KC_DEL,
-        TG(1),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,TD(PY_DOCS),     KC_ENT,
+        TD(ESC_GRAVE_MD_DOCS), KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,   KC_BSPC,   SFTPSCR_PSCR,
+        UNTAB_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,                 KC_DEL,
+        TG(1),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, TD(PY_DOCS),    KC_ENT,
         KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,         KC_UP,
-        KC_LCTL,KC_LGUI,KC_LALT,          KC_SPC,          KC_RALT,KC_LCTL,HOME_END,                     KC_LEFT,KC_DOWN,KC_RIGHT
+        KC_LCTL,KC_LGUI,KC_LALT,          KC_SPC,          KC_RALT,KC_LCTL,END_HOME,                     KC_LEFT,KC_DOWN,KC_RIGHT
     ),
   [1] = LAYOUT(
         TD(ESC_GRAVE_MD_DOCS), KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,     _______,
         _______,_______,_______,_______,_______,START_TERMINAL,START_TYPORA,_______,_______,_______,_______,_______,_______,_______,     _______,
-        TG(1)  ,START_ARDUINO,START_SIGNAL,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_MEDIA_PLAY_PAUSE,
-        _______,_______,START_FILES,START_CHROME,START_VSCODE,_______,_______,START_SPOTIFY,_______,_______,_______,_______,                               KC__VOLUP,
-        _______,_______,_______,                _______,                _______,_______,_______,                  KC_MEDIA_PREV_TRACK,KC__VOLDOWN,KC_MEDIA_NEXT_TRACK
+        TG(1)  ,START_ARDUINO,START_SIGNAL,KC_MS_L,KC_MS_D,_______,_______,KC_MS_U,KC_MS_R,_______,_______,_______,KC_MEDIA_PLAY_PAUSE,
+        _______,_______,START_FILES,START_CHROME,START_VSCODE,_______,_______,START_SPOTIFY,_______,_______,_______,_______,                      KC__VOLUP,
+        _______,_______,_______,                MOUSE_R_L,                _______,_______,_______,                              KC_MEDIA_PREV_TRACK,KC__VOLDOWN,KC_MEDIA_NEXT_TRACK
     )
 };
