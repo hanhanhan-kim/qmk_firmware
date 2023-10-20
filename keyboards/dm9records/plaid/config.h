29cfb24f1d1b9eec71ef55e6ee98e7e260daa619
hanhanhan-kim <mail.han.kim@gmail.com>
2023-10-19 20:27:15 -0700

    update plaid km

--- keyboards/dm9records/plaid/config.h
+++ keyboards/dm9records/plaid/config.h
@@ -130,3 +130,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 // #define BOOTMAGIC_LITE_ROW 0
 // #define BOOTMAGIC_LITE_COLUMN 0
+
+// For a good home row mod experience:
+// Prevent normal rollover on alphas from accidentally triggering mods
+#define IGNORE_MOD_TAP_INTERRUPT 
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat
+// may have to modify this so it's not global
+#define TAPPING_FORCE_HOLD 
+
---
+++ keyboards/dm9records/plaid/keymaps/hanhanhan-kim/keymap.c
@@ -0,0 +1,369 @@
+/* Copyright 2019 Takuya Urakawa (dm9records.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+
+enum plaid_layers {
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+  _NAV
+};
+
+enum plaid_keycodes {
+  QWERTY = SAFE_RANGE,
+  LED_1,
+  LED_2,
+  LED_3,
+  LED_4,
+  LED_5,
+  LED_6,
+  LED_7,
+  LED_8,
+  LED_9,
+  LED_0
+};
+
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+#define ADJUST _ADJUST
+#define NAV_SPACE LT(_NAV, KC_SPC)
+
+// Left-hand home row mods
+#define GUI_A LGUI_T(KC_A)
+#define ALT_S LALT_T(KC_S)
+#define SFT_D LSFT_T(KC_D)
+#define CTL_F LCTL_T(KC_F)
+
+// Right-hand home row mods
+#define CTL_J RCTL_T(KC_J)
+#define SFT_K RSFT_T(KC_K)
+#define ALT_L LALT_T(KC_L)
+#define GUI_SCLN RGUI_T(KC_SCLN)
+
+// array of keys considered modifiers for led purposes
+const uint16_t modifiers[] = {
+    KC_LCTL,
+    KC_RCTL,
+    KC_LALT,
+    KC_RALT,
+    KC_LSFT,
+    KC_RSFT,
+    KC_LGUI,
+    KC_RGUI,
+    LOWER,
+    RAISE,
+    ADJUST
+};
+
+//Setup consts for LED modes
+#define LEDMODE_ON 1 //always on
+#define LEDMODE_OFF 0 //always off
+#define LEDMODE_MODS 2 //On with modifiers
+#define LEDMODE_BLINKIN 3 //blinkinlights - % chance toggle on keypress
+#define LEDMODE_KEY 4 //On with any keypress, off with key release
+#define LEDMODE_ENTER 5 // On with enter key
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Caps |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |SftEnt|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Play | LCtrl| LGUI | LAlt | Lower| Space| Space| Raise| Left | Down |  Up  |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_QWERTY] = LAYOUT_ortho_4x12(
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,        KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
+    KC_ESC,  GUI_A,   ALT_S,   SFT_D,   CTL_F,   KC_G,      KC_H,        CTL_J,   SFT_K,   ALT_L,   GUI_SCLN, KC_QUOT,
+    KC_CAPS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,        KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_SFTENT,
+    KC_MPLY, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   NAV_SPACE, NAV_SPACE,   RAISE,   KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT
+),
+
+/* Lower
+ * ,-----------------------------------------------------------------------------------.
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Pscr |      | Home | End  |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      | Prev | Vol- | Vol+ | Next |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_LOWER] = LAYOUT_ortho_4x12(
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR,  KC_LPRN, KC_RPRN, KC_DEL,
+    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,  KC_PLUS,  KC_LCBR, KC_RCBR, KC_PIPE,
+    KC_CAPS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,  _______,  KC_HOME, KC_END,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______,  KC_MPRV,  KC_VOLD, KC_VOLU, KC_MNXT
+),
+
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Pscr |      | Home | End  |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      | Prev | Vol- | Vol+ | Next |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_RAISE] = LAYOUT_ortho_4x12(
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, _______, KC_HOME,  KC_END, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT
+),
+
+/* Adjust (Lower + Raise)
+ * ,-----------------------------------------------------------------------------------.
+ * |Reset | LED1 | LED2 | LED3 | LED4 | LED5 | LED6 | LED7 | LED8 | LED9 | LED0 |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |AGNORM|AGSWAP|CGSWAP|CGNORM|      |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |EE CLR|      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ADJUST] = LAYOUT_plaid_grid(
+    QK_BOOT, LED_1  , LED_2  , LED_3  , LED_4  , LED_5  , LED_6  , LED_7  , LED_8  , LED_9   , LED_0,   _______ ,
+    _______, _______, _______, _______, _______, AG_NORM, AG_SWAP, CG_SWAP, CG_NORM,  _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
+    EE_CLR , _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+),
+
+/* Navigation
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |LeftCl|MouseL|MouseD|MouseU|MouseR|RiteCl|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      | Left | Down |  Up  |Right |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_NAV] = LAYOUT_ortho_4x12(
+    _______, _______, _______, _______, _______, _______, KC_BTN1, KC_MS_L, KC_MS_D,  KC_MS_U, KC_MS_R, KC_BTN2,
+    _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+),
+
+
+};
+
+// Custom per-key tapping terms
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case GUI_A:
+          return TAPPING_TERM + 10;
+        case GUI_SCLN:
+          return TAPPING_TERM + 10;
+        case ALT_S:
+          return TAPPING_TERM + 5;
+        case ALT_L:
+          return TAPPING_TERM + 5;
+        default:
+          return TAPPING_TERM;
+    }
+}
+
+//Setup config struct for LED
+typedef union {
+  uint32_t raw;
+  struct {
+    uint8_t  red_mode :8;
+    uint8_t  green_mode :8;
+  };
+} led_config_t;
+led_config_t led_config;
+
+//Set leds to saved state during powerup
+void keyboard_post_init_user(void) {
+  // set LED pin modes
+  setPinOutput(LED_RED);
+  setPinOutput(LED_GREEN);
+
+  // Call the post init code.
+  led_config.raw = eeconfig_read_user();
+
+  if(led_config.red_mode == LEDMODE_ON) {
+      writePinHigh(LED_RED);
+  }
+
+  if(led_config.green_mode == LEDMODE_ON) {
+      writePinHigh(LED_GREEN);
+  }
+}
+
+void eeconfig_init_user(void) {  // EEPROM is getting reset! 
+  led_config.raw = 0;
+  led_config.red_mode = LEDMODE_ON;
+  led_config.green_mode = LEDMODE_MODS;
+      eeconfig_update_user(led_config.raw);
+  eeconfig_update_user(led_config.raw);
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+void led_keypress_update(uint8_t led, uint8_t led_mode, uint16_t keycode, keyrecord_t *record) {
+    switch (led_mode) {
+      case LEDMODE_MODS:
+        for (int i=0;i<sizeof(modifiers) / sizeof(modifiers[0]);i++) {
+          if(keycode==modifiers[i]) {
+            if (record->event.pressed) {
+              writePinHigh(led);
+            }
+            else {
+              writePinLow(led);
+            }
+          }
+        }
+        break;
+      case LEDMODE_BLINKIN:
+        if (record->event.pressed) {
+          if(rand() % 2 == 1) {
+            if(rand() % 2 == 0) {
+              writePinLow(led);
+            }
+            else {
+              writePinHigh(led);
+            }
+          }
+        }
+        break;
+      case LEDMODE_KEY:
+        if (record->event.pressed) {
+          writePinHigh(led);
+          return;
+        }
+        else {
+          writePinLow(led);
+          return;
+        }
+        break;
+      case LEDMODE_ENTER:
+        if (keycode==KC_ENT) {
+          writePinHigh(led);
+        }
+        else {
+          writePinLow(led);
+        }
+        break;
+
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  /* If the either led mode is keypressed based, call the led updater
+     then let it fall through the keypress handlers. Just to keep 
+     the logic out of this procedure */
+  if (led_config.red_mode >= LEDMODE_MODS && led_config.red_mode <= LEDMODE_ENTER) {
+      led_keypress_update(LED_RED, led_config.red_mode, keycode, record);
+  }
+  if (led_config.green_mode >= LEDMODE_MODS && led_config.green_mode <= LEDMODE_ENTER) {
+      led_keypress_update(LED_GREEN, led_config.green_mode, keycode, record);
+  }
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        print("mode just switched to qwerty and this is a huge string\n");
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case LED_1:
+      if (record->event.pressed) {
+        if (led_config.red_mode==LEDMODE_ON) {
+            led_config.red_mode=LEDMODE_OFF;
+            writePinLow(LED_RED);
+        }
+        else {
+            led_config.red_mode=LEDMODE_ON;
+            writePinHigh(LED_RED);
+        }
+      }
+      eeconfig_update_user(led_config.raw);
+      return false;
+      break;
+    case LED_2:
+      if (record->event.pressed) {
+        if (led_config.green_mode==LEDMODE_ON) {
+            led_config.green_mode=LEDMODE_OFF;
+            writePinLow(LED_GREEN);
+        }
+        else {
+            led_config.green_mode=LEDMODE_ON;
+            writePinHigh(LED_GREEN);
+        }
+      }
+      eeconfig_update_user(led_config.raw);
+      return false;
+      break;
+    case LED_3:
+      led_config.red_mode=LEDMODE_MODS;
+      eeconfig_update_user(led_config.raw);
+      return false;
+      break;
+    case LED_4:
+      led_config.green_mode=LEDMODE_MODS;
+      eeconfig_update_user(led_config.raw);
+      return false;
+      break;
+    case LED_5:
+      led_config.red_mode=LEDMODE_BLINKIN;
+      eeconfig_update_user(led_config.raw);
+      return false;
+      break;
+    case LED_6:
+      led_config.green_mode=LEDMODE_BLINKIN;
+      eeconfig_update_user(led_config.raw);
+      return false;
+      break;
+    case LED_7:
+      led_config.red_mode=LEDMODE_KEY;
+      eeconfig_update_user(led_config.raw);
+      return false;
+      break;
+    case LED_8:
+      led_config.green_mode=LEDMODE_KEY;
+      eeconfig_update_user(led_config.raw);
+      return false;
+      break;
+    case LED_9:
+      led_config.red_mode=LEDMODE_ENTER;
+      eeconfig_update_user(led_config.raw);
+      return false;
+      break;
+    case LED_0:
+      led_config.green_mode=LEDMODE_ENTER;
+      eeconfig_update_user(led_config.raw);
+      return false;
+      break;
+  }
+  return true;
+}
